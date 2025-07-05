309685c194c9ed78a98fd4c6e7ecb504444c7aef
Juris-Berkulis <krztfjrbhm1@gmail.com>
2025-07-05 23:37:09 +0300

    1. Add a test using PHPUnit's automatic mock creation that verifies that the SQLite repository saves the data to the DB when the user is saved.

--- tests/Blog/Repositories/UsersRepository/SqliteUsersRepositoryTest.php
+++ tests/Blog/Repositories/UsersRepository/SqliteUsersRepositoryTest.php
@@ -5,6 +5,9 @@ namespace JurisBerkulis\GbPhpL2Hw\UnitTests\Blog\Repositories\UsersRepository;
 use JurisBerkulis\GbPhpL2Hw\Blog\Exceptions\InvalidArgumentException;
 use JurisBerkulis\GbPhpL2Hw\Blog\Exceptions\UserNotFoundException;
 use JurisBerkulis\GbPhpL2Hw\Blog\Repositories\UsersRepository\SqliteUsersRepository;
+use JurisBerkulis\GbPhpL2Hw\Blog\User;
+use JurisBerkulis\GbPhpL2Hw\Blog\UUID;
+use JurisBerkulis\GbPhpL2Hw\Person\Name;
 use PDO;
 use PDOStatement;
 use PHPUnit\Framework\MockObject\Exception;
@@ -41,6 +44,49 @@ class SqliteUsersRepositoryTest extends TestCase
 
         // Вызываем метод получения пользователя
         $repository->getByUsername('Ivan');
+    }
+
+    /**
+     * Тест проверяет, что SQLite-репозиторий при сохранении пользователя сохраняет данные в БД
+     * @throws Exception
+     */
+    public function testItSavesUserToDatabase(): void
+    {
+        // Создаём стаб подключения
+        $connectionStub = $this->createStub(PDO::class);
+
+        // Создаём мок запроса, возвращаемый стабом подключения
+        $statementMock = $this->createMock(PDOStatement::class);
+
+        // Описываем ожидаемое взаимодействие репозитория с моком запроса
+        $statementMock
+            // Ожидаем, что будет вызван один раз
+            ->expects($this->once())
+            // метод execute
+            ->method('execute')
+            // с единственным аргументом - массивом
+            ->with([
+                ':uuid' => '123e4567-e89b-12d3-a456-426614174000',
+                ':username' => 'ivan123',
+                ':first_name' => 'Ivan',
+                ':last_name' => 'Nikitin',
+            ]);
+
+        // При вызове метода prepare стаб подключения возвращает мок запроса
+        $connectionStub->method('prepare')->willReturn($statementMock);
+
+        // Передаём в репозиторий стаб подключения
+        $repository = new SqliteUsersRepository($connectionStub);
+
+        // Вызываем метод сохранения пользователя
+        $repository->save(
+            new User(
+                // Свойства пользователя точно такие, как и в описании мока
+                new UUID('123e4567-e89b-12d3-a456-426614174000'),
+                new Name('Ivan', 'Nikitin'),
+                'ivan123',
+            ),
+        );
     }
 
 }
