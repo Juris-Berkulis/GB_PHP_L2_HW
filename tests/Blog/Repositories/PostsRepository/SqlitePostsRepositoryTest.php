47f667cfba5f08c4f73625e9fe79de8010169125
Juris-Berkulis <krztfjrbhm1@gmail.com>
2025-07-06 14:25:33 +0300

    1. Add a test that checks that the SQLite repository saves data to the database when an article is saved.

---
+++ tests/Blog/Repositories/PostsRepository/SqlitePostsRepositoryTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace JurisBerkulis\GbPhpL2Hw\UnitTests\Blog\Repositories\PostsRepository;
+
+use JurisBerkulis\GbPhpL2Hw\Blog\Exceptions\UserNotFoundException;
+use JurisBerkulis\GbPhpL2Hw\Blog\Post;
+use JurisBerkulis\GbPhpL2Hw\Blog\Repositories\PostsRepository\SqlitePostsRepository;
+use JurisBerkulis\GbPhpL2Hw\Blog\Repositories\UsersRepository\UsersRepositoryInterface;
+use JurisBerkulis\GbPhpL2Hw\Blog\User;
+use JurisBerkulis\GbPhpL2Hw\Blog\UUID;
+use JurisBerkulis\GbPhpL2Hw\Person\Name;
+use PDO;
+use PDOStatement;
+use PHPUnit\Framework\MockObject\Exception;
+use PHPUnit\Framework\TestCase;
+
+class SqlitePostsRepositoryTest extends TestCase
+{
+
+    // Функция возвращает объект анонимного класса (в данном случае это стаб),
+    // реализующего контракт UsersRepositoryInterface
+    private function makeUsersRepository(): UsersRepositoryInterface
+    {
+        return new class implements UsersRepositoryInterface {
+
+            public function save(User $user): void
+            {
+            }
+
+            public function get(UUID $uuid): User
+            {
+                throw new UserNotFoundException("Not found");
+            }
+
+            public function getByUsername(string $username): User
+            {
+                throw new UserNotFoundException("Not found");
+            }
+
+        };
+    }
+
+    /**
+     * Тест проверяет, что SQLite-репозиторий при сохранении статьи сохраняет данные в БД
+     * @throws Exception
+     */
+    public function testItSavesPostToDatabase()
+    {
+        // Создаём стаб подключения
+        $connectionStub = $this->createStub(PDO::class);
+
+        // Создаём мок запроса, возвращаемый стабом подключения
+        $statementMock = $this->createMock(PDOStatement::class);
+
+        // Описываем ожидаемое взаимодействие репозитория с моком запроса
+        $statementMock
+            // Ожидаем, что будет вызван один раз
+            ->expects($this->once())
+            // метод execute
+            ->method('execute')
+            // с единственным аргументом - массивом
+            ->with([
+                ':uuid' => '123e4567-e89b-12d3-a456-426614174000',
+                ':user_uuid' => '123e4567-e89b-12d3-a456-426614174001',
+                ':title' => 'Заголовок',
+                ':text' => 'Текст',
+            ]);
+
+        // При вызове метода prepare стаб подключения возвращает мок запроса
+        $connectionStub->method('prepare')->willReturn($statementMock);
+
+        // Передаём в репозиторий стаб подключения
+        $repository = new SqlitePostsRepository($connectionStub, $this->makeUsersRepository());
+
+        // Вызываем метод сохранения пользователя
+        $repository->save(
+            new Post(
+                new UUID('123e4567-e89b-12d3-a456-426614174000'),
+                new User(
+                    new UUID('123e4567-e89b-12d3-a456-426614174001'),
+                    new Name('Ivan', 'Petrov'),
+                    'ivan123',
+                ),
+                'Заголовок',
+                'Текст',
+            )
+        );
+    }
+
+}
