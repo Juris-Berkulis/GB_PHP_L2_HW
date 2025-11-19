784b0859c844d158c0cee6a2cf348f82ec75c2b9
Juris-Berkulis <krztfjrbhm1@gmail.com>
2025-11-19 21:58:46 +0300

    1. Add processing of the HTTP request route to get the user by username.

--- http_api.php
+++ http_api.php
@@ -1,22 +1,61 @@
 <?php
 
+use JurisBerkulis\GbPhpL2Hw\Blog\Exceptions\AppException;
+use JurisBerkulis\GbPhpL2Hw\Blog\Repositories\UsersRepository\SqliteUsersRepository;
+use JurisBerkulis\GbPhpL2Hw\Http\Actions\Users\FindByUsername;
+use JurisBerkulis\GbPhpL2Hw\Http\ErrorResponse;
 use JurisBerkulis\GbPhpL2Hw\Http\Request;
-use JurisBerkulis\GbPhpL2Hw\Http\SuccessfulResponse;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 // Объект запроса из суперглобальных переменных
 $request = new Request($_GET, $_SERVER);
 
-//// Получаем данные из объекта запроса
-//$parameter = $request->query('name');
-//$header = $request->header('Some-Header');
-//$path = $request->path();
+try {
+    // Пытаемся получить путь из запроса
+    $path = $request->path();
+} catch (HttpException) {
+    // Отправляем неудачный ответ, если по какой-то причине не можем получить путь
+    (new ErrorResponse)->send();
 
-// Создаём объект ответа
-$response = new SuccessfulResponse([
-    'message' => 'Hello from PHP!!!',
-]);
+    // Выходим из программы
+    return;
+}
 
-// Отправляем ответ
-$response->send();
+$routes = [
+    // Действие, соответствующее пути /users/show
+    '/users/show' => new FindByUsername(
+        // Действию нужен репозиторий
+        new SqliteUsersRepository(
+            // Репозиторию нужно подключение к БД
+            new PDO('sqlite:' . __DIR__ . '/blog.sqlite')
+        )
+    ),
+//    // Второй маршрут
+//    '/posts/show' => new FindByUuid(
+//        new SqlitePostsRepository(
+//            new PDO('sqlite:' . __DIR__ . '/blog.sqlite')
+//        )
+//    ),
+];
+
+// Если нет маршрута для пути из запроса, отправляем неуспешный ответ
+if (!array_key_exists($path, $routes)) {
+    (new ErrorResponse('Not found'))->send();
+
+    return;
+}
+
+// Выбираем найденное действие
+$action = $routes[$path];
+
+try {
+    // Пытаемся выполнить действие, при этом результатом может быть как успешный, так и неуспешный ответ
+    $response = $action->handle($request);
+
+    // Отправляем ответ
+    $response->send();
+} catch (AppException $e) {
+    // Отправляем неудачный ответ, если что-то пошло не так
+    (new ErrorResponse($e->getMessage()))->send();
+}
