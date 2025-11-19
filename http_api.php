45f4a690211ba3564e79206091509d9c677263d5
Juris-Berkulis <krztfjrbhm1@gmail.com>
2025-11-19 19:05:10 +0300

    1. Added functionality for retrieving a user by username via an HTTP request; 2. Added an interface for actions for processing HTTP requests.

--- http_api.php
+++ http_api.php
@@ -8,10 +8,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 // Объект запроса из суперглобальных переменных
 $request = new Request($_GET, $_SERVER);
 
-// Получаем данные из объекта запроса
-$parameter = $request->query('name');
-$header = $request->header('Some-Header');
-$path = $request->path();
+//// Получаем данные из объекта запроса
+//$parameter = $request->query('name');
+//$header = $request->header('Some-Header');
+//$path = $request->path();
 
 // Создаём объект ответа
 $response = new SuccessfulResponse([
---
+++ src/Http/Actions/Users/FindByUsername.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace JurisBerkulis\GbPhpL2Hw\Http\Actions\Users;
+
+use JurisBerkulis\GbPhpL2Hw\Blog\Exceptions\HttpException;
+use JurisBerkulis\GbPhpL2Hw\Blog\Exceptions\UserNotFoundException;
+use JurisBerkulis\GbPhpL2Hw\Blog\Repositories\UsersRepository\UsersRepositoryInterface;
+use JurisBerkulis\GbPhpL2Hw\Http\Actions\ActionInterface;
+use JurisBerkulis\GbPhpL2Hw\Http\ErrorResponse;
+use JurisBerkulis\GbPhpL2Hw\Http\Request;
+use JurisBerkulis\GbPhpL2Hw\Http\Response;
+use JurisBerkulis\GbPhpL2Hw\Http\SuccessfulResponse;
+
+/**
+ * Класс реализует контракт действия
+ */
+class FindByUsername implements ActionInterface
+{
+
+    // Нам понадобится репозиторий пользователей,
+    // внедряем его контракт в качестве зависимости
+    public function __construct(
+        private UsersRepositoryInterface $usersRepository
+    ) {
+    }
+
+    /**
+     * Вернуть информацию о пользователе по его username
+     * @param Request $request
+     * @return Response
+     */
+    public function handle(Request $request): Response
+    {
+        try {
+            // Пытаемся получить искомое имя пользователя из запроса
+            $username = $request->query('username');
+        } catch (HttpException $e) {
+            // Если в запросе нет параметра username -
+            // возвращаем неуспешный ответ,
+            // сообщение об ошибке берём из описания исключения
+            return new ErrorResponse($e->getMessage());
+        }
+
+        try {
+            // Пытаемся найти пользователя в репозитории
+            $user = $this->usersRepository->getByUsername($username);
+        } catch (UserNotFoundException $e) {
+            // Если пользователь не найден -
+            // возвращаем неуспешный ответ
+            return new ErrorResponse($e->getMessage());
+        }
+
+        // Возвращаем успешный ответ
+        return new SuccessfulResponse([
+            'username' => $user->getUsername(),
+            'name' => $user->getName()->getFirstName() . ' ' . $user->getName()->getLastName(),
+        ]);
+    }
+
+}
