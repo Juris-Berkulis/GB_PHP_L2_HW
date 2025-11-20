f717c11a969aa1476e6955d35a3b9fa8b920be6f
Juris-Berkulis <krztfjrbhm1@gmail.com>
2025-11-20 18:15:58 +0300

    1. Add a test of the functionality of creating an article via an HTTP request.

---
+++ tests/Http/Actions/Posts/CreatePostActionTest.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace JurisBerkulis\GbPhpL2Hw\UnitTests\Http\Actions\Posts;
+
+use JsonException;
+use JurisBerkulis\GbPhpL2Hw\Blog\Exceptions\InvalidArgumentException;
+use JurisBerkulis\GbPhpL2Hw\Blog\Exceptions\PostNotFoundException;
+use JurisBerkulis\GbPhpL2Hw\Blog\Exceptions\UserNotFoundException;
+use JurisBerkulis\GbPhpL2Hw\Blog\Post;
+use JurisBerkulis\GbPhpL2Hw\Blog\Repositories\PostsRepository\PostsRepositoryInterface;
+use JurisBerkulis\GbPhpL2Hw\Blog\Repositories\UsersRepository\UsersRepositoryInterface;
+use JurisBerkulis\GbPhpL2Hw\Blog\User;
+use JurisBerkulis\GbPhpL2Hw\Blog\UUID;
+use JurisBerkulis\GbPhpL2Hw\Http\Actions\Posts\CreatePost;
+use JurisBerkulis\GbPhpL2Hw\Http\ErrorResponse;
+use JurisBerkulis\GbPhpL2Hw\Http\Request;
+use JurisBerkulis\GbPhpL2Hw\Http\SuccessfulResponse;
+use JurisBerkulis\GbPhpL2Hw\Person\Name;
+use PHPUnit\Framework\Attributes\PreserveGlobalState;
+use PHPUnit\Framework\Attributes\RunInSeparateProcess;
+use PHPUnit\Framework\TestCase;
+
+class CreatePostActionTest extends TestCase
+{
+
+    private function assertValidUuid(string $uuid): void
+    {
+        try {
+            new UUID($uuid);
+
+            // Увеличиваем счетчик утверждений (UUID валиден)
+            $this->addToAssertionCount(1);
+        } catch (InvalidArgumentException $e) {
+            $this->fail("Неверный UUID: $uuid - " . $e->getMessage());
+        }
+    }
+
+    public function usersRepository(array $users): UsersRepositoryInterface
+    {
+        return new readonly class($users) implements UsersRepositoryInterface
+        {
+
+            public function __construct(private array $users)
+            {
+            }
+
+            public function save(User $user): void
+            {
+            }
+
+            public function get(UUID $uuid): User
+            {
+                foreach ($this->users as $user) {
+                    if ($user instanceof User && (string)$user->getUuid() === (string)$uuid) return $user;
+                }
+
+                throw new UserNotFoundException("Пользователь не найден: $uuid");
+            }
+
+            public function getByUsername(string $username): User
+            {
+                throw new UserNotFoundException();
+            }
+        };
+    }
+
+    public function postsRepository(): PostsRepositoryInterface
+    {
+        return new class() implements PostsRepositoryInterface
+        {
+
+            public function __construct()
+            {
+            }
+
+            public function save(Post $post): void
+            {
+            }
+
+            public function get(UUID $uuid): Post
+            {
+                throw new PostNotFoundException();
+            }
+
+            public function delete(UUID $uuid): void
+            {
+            }
+
+        };
+    }
+
+    /**
+     * Тест, проверяющий, что будет возвращён удачный ответ
+     * @description Запускаем тест (с помощбю RunInSeparateProcess и PreserveGlobalState) в отдельном процессе
+     * @throws InvalidArgumentException|JsonException
+     */
+    #[RunInSeparateProcess]
+    #[PreserveGlobalState(false)]
+    public function testItReturnsSuccessfulResponse(): void
+    {
+        $authorUuid = '4fcfce3d-10ae-4f9d-8911-c3e156aa957a';
+
+        $request = new Request([], [], '{"author_uuid":"' . $authorUuid . '","text":"some text","title":"some title"}');
+
+        $users = [
+            new User(
+                new UUID($authorUuid),
+                new Name('Ivan', 'Petrov'),
+                'ivan',
+            ),
+        ];
+
+        $postsRepository = $this->postsRepository();
+        $usersRepository = $this->usersRepository($users);
+
+        $action = new CreatePost($postsRepository, $usersRepository);
+        $response = $action->handle($request);
+
+        // Добавьте отладочный вывод
+        if ($response instanceof ErrorResponse) {
+            ob_start();
+            $response->send();
+            $errorOutput = ob_get_clean();
+            echo "Неудачный ответ (ErrorResponse): " . $errorOutput . "\n";
+        }
+
+        // Проверяем, что ответ - удачный
+        $this->assertInstanceOf(SuccessfulResponse::class, $response);
+
+        // Захватываем вывод
+        ob_start();
+        $response->send();
+        $output = ob_get_clean();
+
+        // Парсим JSON и проверяем структуру
+        $responseData = json_decode($output, true);
+
+        // Проверки
+        $this->assertTrue($responseData['success']);
+        $this->assertArrayHasKey('data', $responseData);
+        $this->assertArrayHasKey('uuid', $responseData['data']);
+        $this->assertValidUuid($responseData['data']['uuid']);
+    }
+
+}
